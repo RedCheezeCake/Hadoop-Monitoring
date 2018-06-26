f8578110fe5edb8c5fb25685b7f1bd7547df15cf
Dae-Ho Kim <rlaeogh222@gmail.com>
2018-06-26 11:02:44 +0900

    Update trigger.py

--- Logstash/trigger.py
+++ Logstash/trigger.py
@@ -72,11 +72,11 @@ for line in ls_conf_template :
 
 # launch logstash
 print '[Launch logstash]'
-os.system("nohup "+LS_HOME+'bin/logstash -e \"'+ ls_conf+'\"  > nohup_ls.out&')
+os.system("nohup "+LS_HOME+'bin/logstash -e \"'+ ls_conf+'\" >/dev/null 2>&1 &')
 
 # launch local_collector.py
 print '[Launch local_collector.py]'
-os.system("nohup python "+LS_HOME+'local_collector.py '+db_ip+" "+ db_port+" "+ db_name+" "+ db_user+" "+ db_pass+" "+ cluster_id+" "+ cluster_name+" "+ hostname+'  > nohup_lc.out&')
+os.system("nohup python "+LS_HOME+'local_collector.py '+db_ip+" "+ db_port+" "+ db_name+" "+ db_user+" "+ db_pass+" "+ cluster_id+" "+ cluster_name+" "+ hostname+' >/dev/null 2>&1 &')
 
 # # launch logstash
 # pid=os.fork()
